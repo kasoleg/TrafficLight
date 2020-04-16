24511332e17057b4fa37266f318cd64ad4bed926
kasoleg <kasoleg1@gmail.com>
2020-04-17 00:20:56 +0300

    add millis opportunity

--- src/App.java
+++ src/App.java
@@ -2,13 +2,16 @@ public class App {
     public static void main(final String[] args) {
         new TrafficLight(
                 new TimeCell(
-                    new RedCell()
+                    new RedCell(),
+                    5000
                 ),
                 new TimeCell(
-                    new YellowCell()
+                    new YellowCell(),
+                    1000
                 ),
                 new TimeCell(
-                    new GreenCell()
+                    new GreenCell(),
+                    4000
                 )
         ).run();
     }
--- src/TimeCell.java
+++ src/TimeCell.java
@@ -1,15 +1,17 @@
 public class TimeCell implements Cell {
     private final Cell cell;
+    private final int millis;
 
-    public TimeCell(Cell cell) {
+    public TimeCell(Cell cell, final int millis) {
         this.cell = cell;
+        this.millis = millis;
     }
 
     @Override
     public void run() {
         cell.run();
         try {
-            Thread.sleep(5000);
+            Thread.sleep(millis);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
